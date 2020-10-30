1b0301c3d6de220cea89b1c88f0c253102b7180f
afajardo-sj <andrezffajardo@gmail.com>
2020-10-30 01:02:17 -0500

    Ejemplo de Break con numeros

--- break_continue.py
+++ break_continue.py
@@ -1,8 +1,14 @@
 def run():
-    for contador in range(1000):
-        if contador % 2 != 0:
-            continue
-        print(contador)
+    # for contador in range(1000):
+    #     if contador % 2 != 0:
+    #         continue
+    #     print(contador)
+
+    for i in range(10000):
+        print(i)
+        if i == 5550:
+            break
+
 
 if __name__ == '__main__':
     run()
