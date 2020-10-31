c08aa99e75e7103b287c0d23f5c52c6573773f19
afajardo-sj <andrezffajardo@gmail.com>
2020-10-30 20:21:01 -0500

    Correccion ejercicio prueba numero primo con 1 como no primo

--- prueba_primalidad.py
+++ prueba_primalidad.py
@@ -2,11 +2,9 @@ def es_primo(numero):
     contador = 0
 
     for i in range(1, numero + 1):
-        if i ==1 or i == numero:
-            continue
         if numero % i == 0:
             contador += 1
-    if contador == 0:
+    if contador == 2:
         return True
     else:
         return False
