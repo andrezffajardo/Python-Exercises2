a9718799d4951d42152d1fda6e8e274b904bff39
afajardo-sj <andrezffajardo@gmail.com>
2020-11-06 22:37:31 -0500

    Password Generator in Phyton

---
+++ generador_contrasena.py
@@ -0,0 +1,16 @@
+import ramdom
+
+
+def gen_password():
+    uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'D', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z']
+
+
+
+
+def run():
+    password = gen_password()
+    print('Your new password is: ' + password)
+
+
+    if __name__ == '__main__':
+        run()
