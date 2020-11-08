0bbbc970ba57dc9db2acb0d35b8395245b690d48
afajardo-sj <andrezffajardo@gmail.com>
2020-11-07 23:00:38 -0500

    Password Generator in Phyton run

--- generador_contrasena.py
+++ generador_contrasena.py
@@ -1,10 +1,22 @@
-import ramdom
+import random
 
 
 def gen_password():
-    uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'D', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z']
+    capital_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z']
+    lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    symbols = ['!', '#', '$', '&', '/', '(', ')']
+    numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
+    characters = capital_letters + lowercase + symbols + numbers
 
+    password = []
+
+    for i in range(15):
+        character_random = random.choice(characters)
+        password.append(character_random)
+
+    password = "".join(password)
+    return password
 
 
 def run():
@@ -12,5 +24,5 @@ def run():
     print('Your new password is: ' + password)
 
 
-    if __name__ == '__main__':
-        run()
+if __name__ == '__main__':
+    run()
